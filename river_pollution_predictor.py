1a50a322b4d3e01a95aa55021706be22dd74b488
lyhernkeith <68096948+lyhernkeith@users.noreply.github.com>
2026-01-04 15:49:36 +0800

    fix

--- river_pollution_predictor.py
+++ river_pollution_predictor.py
@@ -141,7 +141,8 @@ app.layout = html.Div([
     )
 ])
 
+server = app.server
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 8050))
-#     app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port)
