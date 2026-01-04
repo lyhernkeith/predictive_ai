a83aa87b7ded6a14d3eb04867ec86e50e711a102
lyhernkeith <68096948+lyhernkeith@users.noreply.github.com>
2026-01-04 17:16:13 +0800

    dsfg

--- river_pollution_predictor.py
+++ river_pollution_predictor.py
@@ -124,8 +124,8 @@ app.layout = html.Div([
     )
 ])
 
-server = app.server
+# server = app.server
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))
-    app.run(debug=False, host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8050))
+#     app.run(debug=False, host="0.0.0.0", port=port)
